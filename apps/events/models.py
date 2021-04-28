20ade40d034f676dea82b957ad99af793198ec3a
n1EzeR <erasylz@gmail.com>
2021-04-28 17:06:36 +0300

    Add FK user to evens

--- apps/events/models.py
+++ apps/events/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 from apps.steps.models import Steps
+from apps.users.models import User
 
 
 class Events(models.Model):
@@ -13,3 +14,4 @@ class Events(models.Model):
     time = models.DateTimeField(default=timezone.now)
     action_id = models.TextField(choices=Actions.choices)
     target_id = models.ForeignKey(Steps, null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
