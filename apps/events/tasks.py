253cc974a1e9985762286959398df3f5475b14c3
n1EzeR <erasylz@gmail.com>
2021-04-29 12:39:00 +0300

    Add comments to ch task

--- apps/events/tasks.py
+++ apps/events/tasks.py
@@ -18,11 +18,13 @@ STEPIK_FOUNDED_DATETIME = datetime.datetime(2013, 9, 1, tzinfo=pytz.UTC)
 
 @celery_app.task
 def load_to_ch():
+    """Load consolidated events data from postgres to clickhouse every 5 minutes."""
+
     last_parsed = STEPIK_FOUNDED_DATETIME  # suppose we dynamically get this from db
     logger.info(f"Started data collection starting from {last_parsed}")
 
-    events: QuerySet = Events.objects.select_related("user")
-    insert_values = events.filter(time__gt=last_parsed).values_list(
+    events: QuerySet = Events.objects.select_related("user")  # join tables
+    insert_values = events.filter(time__gt=last_parsed).values_list(  # as tuples
         "time",
         "user_id",
         "user__join_date",
